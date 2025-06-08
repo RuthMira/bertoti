c0400bdd7450fc381ace151cf2a5357efcf40380
RuthMira <143049504+RuthMira@users.noreply.github.com>
2025-06-08 19:21:51 -0300

    Refactor Strategy pattern implementation and update vehicle movement methods

--- PPS_padroesprojetos/Stategy/StrategyAntiPattern.java
+++ PPS_padroesprojetos/Stategy/StrategyAntiPattern.java
@@ -1,18 +1,39 @@
 package PPS_padroesprojetos.Stategy;
+
+class Veiculo {
+    public void abastecer() {
+        System.out.println("Abastecendo veículo...");
+    }
+}
+
+class Carro extends Veiculo {
+    public void mover() {
+        System.out.println("Andando com 4 rodas");
+    }
+}
+
+class Moto extends Veiculo {
+    public void mover() {
+        System.out.println("Andando com 2 rodas");
+    }
+}
+
+class Aviao extends Veiculo {
+    public void mover() {
+        System.out.println("Voando pelos céus");
+    }
+}
+
 public class StrategyAntiPattern {
     public static void main(String[] args) {
-        executar("A");
-        executar("B");
-        executar("C");
-    }
+        Veiculo v1 = new Carro();
+        Veiculo v2 = new Moto();
+        Veiculo v3 = new Aviao();
 
-    public static void executar(String tipo) {
-        if (tipo.equals("A")) {
-            System.out.println("Executando estratégia A");
-        } else if (tipo.equals("B")) {
-            System.out.println("Executando estratégia B");
-        } else {
-            System.out.println("Tipo inválido");
-        }
+        v1.abastecer(); v2.abastecer(); v3.abastecer();
+
+        ((Carro)v1).mover();
+        ((Moto)v2).mover();
+        ((Aviao)v3).mover();
     }
 }
--- PPS_padroesprojetos/Stategy/StrategyPattern.java
+++ PPS_padroesprojetos/Stategy/StrategyPattern.java
@@ -1,26 +1,56 @@
 package PPS_padroesprojetos.Stategy;
-interface Strategy {
-    void execute();
+
+interface ModoDeMover {
+    void mover();
 }
 
-class StrategyA implements Strategy {
-    public void execute() {
-        System.out.println("Executando estratégia A");
+class Dirigir implements ModoDeMover {
+    public void mover() {
+        System.out.println("Dirigindo pelas ruas");
     }
 }
 
-class StrategyB implements Strategy {
-    public void execute() {
-        System.out.println("Executando estratégia B");
+class Pilotar implements ModoDeMover {
+    public void mover() {
+        System.out.println("Voando com o avião");
+    }
+}
+
+class Pedalar implements ModoDeMover {
+    public void mover() {
+        System.out.println("Pedalando a bicicleta");
+    }
+}
+
+class Veiculo {
+    private ModoDeMover modo;
+
+    public void setModoDeMover(ModoDeMover modo) {
+        this.modo = modo;
+    }
+
+    public void locomover() {
+        modo.mover();
+    }
+
+    public void abastecer() {
+        System.out.println("Abastecendo veículo...");
     }
 }
 
 public class StrategyPattern {
     public static void main(String[] args) {
-        Strategy strategy = new StrategyA();
-        strategy.execute();
+        Veiculo v = new Veiculo();
 
-        strategy = new StrategyB();
-        strategy.execute();
+        v.abastecer();
+
+        v.setModoDeMover(new Dirigir());
+        v.locomover();
+
+        v.setModoDeMover(new Pilotar());
+        v.locomover();
+
+        v.setModoDeMover(new Pedalar());
+        v.locomover();
     }
 }
